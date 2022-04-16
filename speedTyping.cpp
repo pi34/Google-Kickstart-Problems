9084e9fb6deb517e5219df7694eb31ea779e8c5f
Riya Manchanda <59563727+pi34@users.noreply.github.com>
2022-04-16 12:32:55 +0530

    Create speedTyping.cpp

---
+++ speedTyping.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main ()
+{
+    int t;
+    cin >> t;
+    
+    for (int i = 1; i <= t; i++) {
+        string I, P;
+        cin >> I >> P;
+        
+        int n = I.length();
+        int m = P.length();
+        
+        int ptI = 0;
+        int ptP = 0;
+        
+        while (ptI < n && ptP < m) {
+            if (I[ptI] == P[ptP]) {
+                ptI++;
+                ptP++;
+            } else {
+                ptP++;
+            }
+        }
+        
+        if (ptI == n) {
+            cout << "Case #" << i << ": " << m-n << "\n";
+        } else {
+            cout << "Case #" << i << ": " << "IMPOSSIBLE" << "\n";
+        }
+    }
+}
